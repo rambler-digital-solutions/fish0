7d4b64360eda4b7e2a0dd5c2327706085dfb16b8
Gorgolevskiy Pavel <p.gorgolevskiy@rambler-co.ru>
2017-06-01 14:32:56 +0300

    if config.mongo_uri is not empty mongo_reader will use it and ignore config.mongo_hosts

--- lib/fish0/configuration.rb
+++ lib/fish0/configuration.rb
@@ -3,23 +3,29 @@ module Fish0
   # Fish0::Configuration
   #
   # Usage:
+  #   Fish0 will use mongo_uri if it's not empty and ignore mongo_hosts:
   #   # config/initializers/fish0.rb
   #   Fish0::Configuration.configure do |config|
-  #     config.mongo_hosts = ['localhost:27017']
-  #     config.mongo_params = { database: 'your_data_development', read: { mode: :secondary } }
-  #   end
-  #
-  # Or if you want to use full mongo uri for access:
-  #   # config/initializers/fish0.rb
-  #   Fish0::Configuration.configure do |config|
-  #     config.use_uri_format = true
   #     config.mongo_uri = 'mongodb://user:password@host_1:27017,replica_host_2:27017/'\
   #       'fish0_development?auth_source=admin'
   #     config.mongo_params = { read: { mode: :secondary } }
   #   end
   #
+  # Or if you want to use array of hosts:
+  #   # config/initializers/fish0.rb
+  #   Fish0::Configuration.configure do |config|
+  #     config.mongo_hosts = ['localhost:27017']
+  #     config.mongo_params = { database: 'your_data_development', read: { mode: :secondary } }
+  #   end
+
   class Configuration
     include ActiveSupport::Configurable
+
+    # Usage:
+    # Enter your full mongo_uri (can include username, password, multiple hosts, ports, db name,
+    # and additional options as get-parameters
+    # NOTE: do not set default value here - it will break projects which don't use mongo_uri format
+    config_accessor :mongo_uri
 
     config_accessor :mongo_hosts do
       ['localhost:27017']
@@ -27,23 +33,10 @@ module Fish0
 
     # Usage:
     # Enter your database and another params as second argument to MongoClient::New
-    ##
+    # In case of using mongo_uri - no need to set database here,
+    # but database set here will override the one set in mongo_uri
     config_accessor :mongo_params do
       { database: 'fish0_development', read: { mode: :secondary } }
-    end
-
-    # Usage:
-    # Fish0 will use Configuration.mongo_uri only if Configuration.use_uri_format is set to 'true'
-    ##
-    config_accessor :use_uri_format do
-      false
-    end
-
-    # Usage:
-    # Enter your full mongo_uri (can include username, password, multiple hosts, ports, db name,
-    # and additional options as get-parameters
-    config_accessor :mongo_uri do
-      'mongodb://localhost:27017/fish0_development'
     end
   end
 end
